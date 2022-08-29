05b59511a27c1d162e9020b9cdcd700a35eb5a7f
jayjunn <jayjunn@outlook.com>
2022-08-29 16:37:21 +0900

    Design: add outline style buttons

--- packages/@aergo-connect/lib-ui/src/buttons/types.ts
+++ packages/@aergo-connect/lib-ui/src/buttons/types.ts
@@ -1,6 +1,19 @@
 const tuple = <T extends string[]>(...args: T) => args;
 
+<<<<<<< Updated upstream
 export const ButtonTypes = tuple('default', 'primary', 'secondary', 'danger');
+=======
+export const ButtonTypes = tuple(
+  "default",
+  "primary",
+  "primary-outline",
+  "secondary",
+  "secondary-outline",
+  "gradation",
+  "font-gradation",
+  "white"
+);
+>>>>>>> Stashed changes
 export type ButtonType = typeof ButtonTypes[number];
 
 export const ButtonSizes = tuple('default');

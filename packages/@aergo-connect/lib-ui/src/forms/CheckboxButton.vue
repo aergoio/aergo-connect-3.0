1f717a46f31d28c1ffea61a9568d8c5872b99107
jayjunn <jayjunn@outlook.com>
2022-08-30 18:06:39 +0900

    Design: add Radio & checkbox button

---
+++ packages/@aergo-connect/lib-ui/src/forms/CheckboxButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="checkbox" class="checkbox" :disabled="disabled" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style lang="scss">
+@import "../styles/variables";
+
+.checkbox {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 1px solid #c9c9c9;
+  cursor: pointer;
+  accent-color: $Blue01;
+  border-radius: 2pt;
+}
+</style>

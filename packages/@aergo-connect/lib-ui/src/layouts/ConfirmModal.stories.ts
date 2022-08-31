1a7fa6e33d2c91145fb42f7b641dbea0146904ac
jayjunn <jayjunn@outlook.com>
2022-08-31 16:24:27 +0900

    Design: add ConfirmModal UI

---
+++ packages/@aergo-connect/lib-ui/src/layouts/ConfirmModal.stories.ts
@@ -0,0 +1,31 @@
+import { storiesOf } from "@storybook/vue";
+import ConfirmModal from "./ConfirmModal.vue";
+import { Button } from "../buttons";
+import { ButtonGroup } from "../buttons";
+import { text } from "@storybook/addon-knobs";
+
+storiesOf("Layouts/ConfirmModal", module).add("basic", () => ({
+  components: { ConfirmModal, Button, ButtonGroup },
+
+  template: `
+  <ConfirmModal :name="name" :title="title">
+   <Button type="primary" size="medium">Add</Button>
+  <ConfirmModal/> 
+`,
+
+  props: {
+    name: {
+      type: String,
+      default: text("name", ""),
+    },
+    title: {
+      type: String,
+      default: text("Title", ""),
+    },
+  },
+}));
+
+// <ButtonGroup vertical="true">
+// <Button type="primary" size="medium">Add</Button>
+// <Button type="primary-outline" size="medium">Cancel</Button>
+// <ButtonGroup/>
---
+++ packages/@aergo-connect/lib-ui/src/layouts/ConfirmModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <Icon :name="`${name}`" :size="100" />
+    <h1>{{ title }}</h1>
+    <Button :type="`${buttonType}`" size="medium">OK</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Icon from "../icons/Icon.vue";
+import Button from "../buttons/Button.vue";
+
+export default Vue.extend({
+  components: { Icon, Button },
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    buttonType: {
+      type: String,
+      default: "gradation",
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+@import "../styles/mixin.scss";
+
+.wrapper {
+  max-width: 317px;
+  max-height: 300px;
+  box-shadow: 0px 5px 12px rgba(0, 0, 0, 0.05);
+  border-radius: 8px;
+  @include flex__column__center;
+  padding: 30px 14px 20px 14px;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+
+  h1 {
+    text-align: center;
+    word-wrap: break-word;
+    width: 200px;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 30px;
+    margin: 30px 0 43px 0;
+  }
+}
+</style>

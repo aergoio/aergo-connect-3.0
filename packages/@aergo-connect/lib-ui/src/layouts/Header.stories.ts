717af709c44c3be64dd5d93057ac02b9fae6f9f2
ImHun <load1999@gmail.com>
2022-09-06 17:39:41 +0900

    style : 헤더 추가

--- packages/@aergo-connect/lib-ui/src/layouts/Header.stories.ts
+++ packages/@aergo-connect/lib-ui/src/layouts/Header.stories.ts
@@ -8,7 +8,7 @@ import Icon from "../icons/Icon.vue";
 //import InvertedColors from '../theme/InvertedColors.vue';
 
 storiesOf("Layouts/Header", module).add("basic", () => ({
-  components: { ScrollView, Header, HeaderLogo, Icon },
+  components: { ScrollView, Header, HeaderLogo },
   template: `
       <div style="width: 360px; height: 400px; border-radius: 8px; box-shadow: 0 16px 24px 0 rgba(0, 0, 0, 0.08);">
         <ScrollView>

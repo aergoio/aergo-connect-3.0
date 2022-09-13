4e9627a3941e6179a3ccbebbadf00acd41ee4a4b
jayjunn <jayjunn@outlook.com>
2022-09-13 17:39:06 +0900

    update: import/format 업데이트

--- packages/@aergo-connect/extension/src/views/accounts/import/2-Format.vue
+++ packages/@aergo-connect/extension/src/views/accounts/import/2-Format.vue
@@ -1,21 +1,30 @@
 <template>
   <ScrollView class="page">
-    <div class="content">
-      <section class="dialog-header">
+    <Header button="back" title="Import Account" />
+    <div class="foramt-content">
+      <!-- <section class="dialog-header">
         <BackButton :to="{ name: 'account-import' }" />
-      </section>
-      <Heading>Import Format</Heading>
-      <p>Please select the import format.</p>
-      <p>
-        <ButtonGroup vertical>
-          <Button type="primary" :to="{ name: 'account-import-keystore' } ">Keystore File</Button>
-          <Button :to="{ name: 'account-import-wif' } ">Encrypted String</Button>
-          <Button :to="{ name: 'account-import-seedphrase' } ">Mnemonic Seedphrase</Button>
-        </ButtonGroup>
+      </section> -->
+      <!-- <Heading>Import Format</Heading> -->
+      <span class="preheaer">Please select the import format.</span>
+      <p class="buttons">
+        <Button type="gradation" size="large" :to="{ name: 'account-import-seedphrase' }"
+          >Mnemonic Seedphrase</Button
+        >
+        <Button type="gradation" size="large" :to="{ name: 'account-import-wif' }"
+          >Encrypted Private Key</Button
+        >
+        <Button type="gradation" size="large" :to="{ name: 'account-import-keystore' }"
+          >Keystore File</Button
+        >
       </p>
       <p class="note">
-        "Keystore File" is the recommended format. Use it when you saved your key as an encrypted file.<br>
-        "Encrypted String" is a legacy format. Use it when you copied your key as an encrypted string.
+        <span>Keystore File</span> is the recommended format. Use it when you saved your key as an
+        encrypted file.
+        <br />
+        <br />
+        <span>Encrypted String</span> is a legacy format. Use it when you copied your key as an
+        encrypted string.
       </p>
     </div>
   </ScrollView>
@@ -23,10 +32,10 @@
 
 <script lang="ts">
 import { BackButton, Button, ButtonGroup } from '@aergo-connect/lib-ui/src/buttons';
-import { ScrollView } from '@aergo-connect/lib-ui/src/layouts';
+import { ScrollView, Header } from '@aergo-connect/lib-ui/src/layouts';
 import Heading from '@aergo-connect/lib-ui/src/content/Heading.vue';
 
-import Component, { mixins } from 'vue-class-component'
+import Component, { mixins } from 'vue-class-component';
 
 @Component({
   components: {
@@ -35,12 +44,47 @@ import Component, { mixins } from 'vue-class-component'
     Heading,
     Button,
     ButtonGroup,
+    Header,
   },
 })
-export default class Import extends mixins() {
-}
+export default class Import extends mixins() {}
 </script>
 
 <style lang="scss">
+.foramt-content {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
 
+  .preheaer {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 25px;
+    letter-spacing: -0.333333px;
+    color: #231f20;
+    margin-top: 32px;
+    margin-bottom: 62px;
+  }
+
+  .buttons {
+    margin-bottom: 32px;
+    button {
+      margin-bottom: 28px;
+    }
+  }
+
+  .note {
+    width: 327px;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 19px;
+    letter-spacing: -0.333333px;
+    color: #686767;
+    span {
+      color: #279ecc;
+    }
+  }
+}
 </style>

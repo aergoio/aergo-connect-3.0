9383676a1f5caa1ef1eaf1a3ad42d1b5eb2211f9
ImHun <load1999@gmail.com>
2022-09-19 13:32:26 +0900

    style : create 스타일추가 및 데이터바인딩

--- packages/@aergo-connect/extension/src/views/accounts/Create.vue
+++ packages/@aergo-connect/extension/src/views/accounts/Create.vue
@@ -1,33 +1,27 @@
 <template>
   <ScrollView>
-    <Identicon :text="address" class="circle" />
-    <Heading class="big-title">Registered Account!</Heading>
-
-    <div>
+    <div class="content">
+      <Identicon :text="address" class="circle" />
+      <Heading class="big-title">Registered Account!</Heading>
       <span>Address</span>
-      <div>
-        <span>{{ address }}</span>
-      </div>
-    </div>
 
-    <div>
-      <div>
-        <span>Nickname</span>
-        <!-- <img src="@aergo-connect/lib-ui/src/icons/img/edit.svg" alt="" /> -->
-        <!-- <Icon :name=" ? 'logo' : 'network-other'" :size="36" /> -->
-        <div>
-          <span>ACCOUNT 1</span>
-        </div>
+      <div class="address">
+        <span class="address_hash">{{ address }}</span>
       </div>
+      <span class="nickname">Nickname</span>
+
+      <div class="user_nickname">
+        <span class="user_nickname_text">ACCOUNT 1</span>
+      </div>
+      <ButtonGroup vertical>
+        <Button type="primary-outline" size="large" :to="{ name: 'account-backup' }">
+          Backup Private Key
+        </Button>
+        <Button :to="{ name: 'accounts-list' }" type="primary" size="large">
+          Home
+        </Button>
+      </ButtonGroup>
     </div>
-    <ButtonGroup vertical>
-      <Button type="primary-outline" size="large" :to="{ name: 'account-backup' }">
-        Backup Private Key
-      </Button>
-      <Button :to="{ name: 'accounts-list' }" type="primary" size="large">
-        Home
-      </Button>
-    </ButtonGroup>
   </ScrollView>
 </template>
 
@@ -52,7 +46,76 @@ export default class Create extends mixins() {
   address = '';
   chainId = '';
   mnemonic = '';
+  async beforeMount() {
+    const { address, chainId } = await this.$route.params;
+    this.address = address;
+    this.chainId = chainId;
+  }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .circle {
+    margin-top: 56px;
+    width: 59px;
+    height: 59px;
+  }
+  .big-title {
+    margin-top: 15px;
+    margin-bottom: 30px;
+  }
+  .address {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 283px;
+    height: 60px;
+    background: #eff5f7;
+    opacity: 0.7;
+    border-radius: 4px;
+    .address_hash {
+      width: 231px;
+      word-break: break-all;
+      font-family: 'Outfit';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 15px;
+      line-height: 19px;
+      letter-spacing: -0.333333px;
+      color: #279ecc;
+    }
+  }
+  .nickname {
+    margin-top: 22px;
+    margin-bottom: 7px;
+  }
+  .user_nickname {
+    display: flex;
+    align-items: center;
+    width: 283px;
+    height: 41px;
+    background: #eff5f7;
+    opacity: 0.7;
+    border-radius: 4px;
+    margin-bottom: 67px;
+    .user_nickname_text {
+      margin-left: 20px;
+      width: 200px;
+      word-break: break-all;
+      font-family: 'Outfit';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 15px;
+      line-height: 19px;
+      letter-spacing: -0.333333px;
+      color: #686767;
+    }
+  }
+}
+</style>

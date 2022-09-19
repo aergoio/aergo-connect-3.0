4f5d9bb1ba54a6984d30b6f64e3e7f50d603bce3
jayjunn <jayjunn@outlook.com>
2022-09-19 13:30:10 +0900

    add: ui/sideNavButton 추가

---
+++ packages/@aergo-connect/lib-ui/src/buttons/SideNavButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="nav__button">
+    <div class="nav-button-icon">
+      <Icon :name="`${img}`" />
+    </div>
+    <span class="address">{{ title }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import Icon from '../icons/Icon.vue';
+import { RawLocation } from 'vue-router';
+
+export default Vue.extend({
+  components: { Icon },
+  props: {
+    img: {
+      type: String,
+      default: 'security',
+    },
+    title: {
+      type: String,
+      default: 'test',
+    },
+    to: {
+      type: [String, Object] as PropType<RawLocation>,
+    },
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+.nav__button {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  cursor: pointer;
+  transition: 0.3s;
+  max-width: 233px;
+  svg {
+    transition: 0.3s;
+  }
+  &:hover {
+    background: #f6f6f6;
+    border-radius: 4px;
+    color: #279ecc;
+    svg {
+      filter: invert(59%) sepia(89%) saturate(1935%) hue-rotate(163deg) brightness(87%)
+        contrast(82%);
+    }
+  }
+  .nav-button-icon {
+    margin: 0 13px 0 13px;
+  }
+}
+</style>

6d6898fa1c264b38dc76ce16eadc1bd869573362
ImHun <load1999@gmail.com>
2022-11-01 13:30:41 +0900

    style : loading bar 수정

--- packages/@aergo-connect/lib-ui/src/forms/LoadingBar.vue
+++ packages/@aergo-connect/lib-ui/src/forms/LoadingBar.vue
@@ -1,10 +1,12 @@
 <template>
   <!--  <div class="lds-facebook" v-if="loading">
 -->
-  <div class="lds-facebook">
-    <div></div>
-    <div></div>
-    <div></div>
+  <div class="loading_backdrop">
+    <div class="lds-facebook">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
@@ -13,12 +15,21 @@ export default {};
 </script>
 
 <style>
+.loading_backdrop {
+  position: absolute;
+  width: 375px;
+  height: 600px;
+  left: 0px;
+  top: 0px;
+  background: rgba(0, 0, 0, 0.1);
+  z-index: 1;
+}
 .lds-facebook {
   display: inline-block;
-  position: fixed;
+  position: absolute;
   width: 64px;
   height: 64px;
-  top: 50%;
+  bottom: 50%;
   left: 40%;
 }
 
@@ -27,7 +38,7 @@ export default {};
   position: absolute;
   left: 6px;
   width: 13px;
-  background: #42b883;
+  background: #279ecc;
   animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
 }
 

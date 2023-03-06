c5306630fc84392cfc094d559289a9223da5ca63
ImHun <load1999@gmail.com>
2023-03-06 10:53:03 +0900

    feat : postMessage 코드 추가

--- packages/@aergo-connect/extension/src/content-script.js
+++ packages/@aergo-connect/extension/src/content-script.js
@@ -14,3 +14,16 @@ function connect() {
   port.onDisconnect.addListener(connect);
 }
 connect();
+
+// Message from website to extension
+window.addEventListener('message', (event) => {
+  // We only accept messages from ourselves
+  if (event.source != window) {
+    return;
+  }
+
+  if (event.data.type && event.data.type == 'AERGO_REQUEST') {
+    // console.log("[Contentscript] Received message from website", event.data);
+    port.postMessage(event.data);
+  }
+});
